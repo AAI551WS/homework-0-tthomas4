1b4edf89fb13856320e342bc4f03cc06a049d42c
tthomas4 <157071314+tthomas4@users.noreply.github.com>
2024-01-24 02:40:37 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,14 +1,14 @@
 def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
-    """
+    """This function saya "Hello World! This is my first time using Python!" 
 
     """
 
     # Write a comment; comments are very useful to other programmers.
 
     # Write a Python statement to print Hello, World!
-
+    #I've never used Python and coding isn't my speciality. Here to learning! 
 def add(a,b):
 
     # Write simple code to add variable a and b
