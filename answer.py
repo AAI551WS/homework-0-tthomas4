21e8d3926305ccd88b94e0ae0afe3a7a1c17aa62
tthomas4 <157071314+tthomas4@users.noreply.github.com>
2024-01-24 03:01:24 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -9,11 +9,11 @@ def hello_world():
 
     # Write a Python statement to print Hello, World!
     # I've never used Python and coding isn't my speciality. Here to learning! 
-    # I don't really understand how to write simple code to add variable a and b below 
+    # I don't understand how to write simple code to add variable a and b below. 
 
 def add(a,b): 
 
     # Write simple code to add variable a and b
-    output = 
+    output = a + b 
 
     return output
